2a001d341437ec760035b7b9a5c5ad9f64b6d0e7
Hanlin Yu <yuhanlin611@gmail.com>
2022-08-20 15:00:00 -0700

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let tempArray = [];
+    let temp1 = 0;
+    let temp2 = 0;
+    if (num1 > num2) {
+        temp1 = num2;
+        temp2 = num1;
+    }
+    else {
+        temp1 = num1;
+        temp2 = num2;
+    }
+
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    if (typeof num1 != "number" || typeof num2 != "number") return "ERROR";
+
+    for (let i = temp1; i <= temp2; i++) {
+        tempArray.push(i);
+    }
+    for (const num of tempArray) {
+        sum += num;
+    }
+    return sum;
 
 };
 
