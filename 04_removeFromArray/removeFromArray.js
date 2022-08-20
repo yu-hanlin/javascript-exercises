797c8c87db7a5b30a5830cc2265e280724502bd9
Hanlin Yu <yuhanlin611@gmail.com>
2022-08-20 14:46:16 -0700

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function (inputArray, toRemove) {
+    let outputArray = [];
+    const tempArray = Array.prototype.slice.call(arguments);
+    let removeArray = tempArray.slice(1, tempArray.length);
+    let includeBool = true;
+    for (let i = 0; i < inputArray.length; i++) {
+        for (let j = 0; j < removeArray.length; j++) {
+            if (inputArray[i] === removeArray[j]) {
+                includeBool = false;
+                break;
+            }
+            else {
+                includeBool = true;
+            }
+        }
+        if (includeBool) {
+            outputArray.push(inputArray[i]);
+        }
+
+    }
+    return outputArray;
 
 };
 
