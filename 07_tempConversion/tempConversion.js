c918081b92ec3056f02f3075c094021fecc4cd2a
Hanlin Yu <yuhanlin611@gmail.com>
2022-08-20 15:16:21 -0700

    Finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(input) {
+  return Math.round((input - 32) * (5/9) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(input) {
+  return Math.round((input * (9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
