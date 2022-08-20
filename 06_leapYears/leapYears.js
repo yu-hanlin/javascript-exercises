57f488a67907d903d14920e35ea722b3011f545b
Hanlin Yu <yuhanlin611@gmail.com>
2022-08-20 15:10:36 -0700

    Finish exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(years) {
+    if (years % 4 === 0) {
+        if (years % 100 === 0 && years % 400 === 0) {
+            return true;
+        }
+        else if (years % 100 === 0) {
+            return false;
+        }
+        return true;
+    }
+    else return false;
 };
 
 // Do not edit below this line
