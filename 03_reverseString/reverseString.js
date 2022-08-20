018e5fc3b284c1f69fbf492861d657071886881e
Hanlin Yu <yuhanlin611@gmail.com>
2022-08-20 14:04:52 -0700

    Finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,10 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    let output = "";
+    let tempArray = input.split("");
+    for (let i = input.length - 1; i >= 0; i--) {
+        output += tempArray[i];
+    }
+    return output;
 
 };
 
